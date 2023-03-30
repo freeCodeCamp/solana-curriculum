67bf3a96cbfb768804caa3e4f98d14ff0683dc53
Shaun Hamilton <shauhami020@gmail.com>
2023-03-30 20:28:25 +0100

    review: remove _answer folders

--- learn-the-metaplex-sdk-by-minting-an-nft/_answer/spl-program/get-token-info.js
+++
@@ -1,9 +0,0 @@
-import { Connection } from '@solana/web3.js';
-import { getMint } from '@solana/spl-token';
-import { mintAddress } from './utils.js';
-
-const connection = new Connection('http://127.0.0.1:8899');
-
-const mint = await getMint(connection, mintAddress);
-
-console.log(mint);
