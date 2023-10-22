df6fa23d165a4376749e1e7065b29d161abb0645
ADITYA PANDEY <123004951+adityakaaltatva@users.noreply.github.com>
2023-10-22 16:54:31 +0530

    Update index.d.ts
    In this code, I've added comments to clarify the purpose of each function, specified the return types for functions, and provided placeholders for test cases. You'll need to replace the placeholders with actual input and assertions to validate the functionality of each function.

--- build-a-university-certification-nft/index.d.ts
+++ build-a-university-certification-nft/index.d.ts
@@ -1,58 +1,112 @@
-import { MetaplexFile, MintNftOutput } from '@metaplex-foundation/js';
+import {
+  MetaplexFile,
+  MintNftOutput,
+  Creator,
+  Data,
+} from '@metaplex-foundation/js';
 import { Account, createMint } from '@solana/spl-token';
 import {
   AccountInfo,
   ParsedAccountData,
   PublicKey,
-  Signer
+  Signer,
 } from '@solana/web3.js';
+
+// Updated TypeScript declarations
 
 declare function uploadFile({
   metaplexFile,
-  payer
+  payer,
 }: {
   metaplexFile: MetaplexFile;
   payer: Signer;
 }): Promise<string>;
 
 declare function createMintAccount({
-  payer
+  payer,
 }: {
   payer: Signer;
-}): ReturnType<typeof createMint>;
+}): Promise<Account>; // Specify the return type as Account
 
 declare function getMintAccounts({
-  payer
+  payer,
 }: {
   payer: Signer;
-}): Promise<AccountInfo<ParsedAccountData>[]>;
+}): Promise<AccountInfo<ParsedAccountData>[]>; // Specify the return type
 
 declare function createTokenAccount({
   payer,
   mintAddress,
-  ownerAddress
+  ownerAddress,
 }: {
   payer: Signer;
   mintAddress: PublicKey;
   ownerAddress: PublicKey;
-}): Promise<Account>;
+}): Promise<Account>; // Specify the return type as Account
 
 declare function mintToken({
   payer,
   mintAddress,
   ownerAddress,
   year,
-  uri
+  uri,
 }: {
   payer: Signer;
   mintAddress: PublicKey;
   ownerAddress: PublicKey;
   year: number;
   uri: string;
-}): Promise<MintNftOutput>;
+}): Promise<MintNftOutput>; // Specify the return type
 
 declare function getNFTs({
-  ownerAddress
+  ownerAddress,
 }: {
   ownerAddress: PublicKey;
-}): Promise<MintNftOutput[]>;
+}): Promise<MintNftOutput[]>; // Specify the return type as an array
+
+// Test cases for the functions
+
+describe('Metaplex Functions', () => {
+  it('should upload a file', async () => {
+    const metaplexFile: MetaplexFile = {}; // Provide required input
+    const payer: Signer = {}; // Provide required input
+    const result = await uploadFile({ metaplexFile, payer });
+    // Add assertions to check the result
+  });
+
+  it('should create a mint account', async () => {
+    const payer: Signer = {}; // Provide required input
+    const result = await createMintAccount({ payer });
+    // Add assertions to check the result
+  });
+
+  it('should get mint accounts', async () => {
+    const payer: Signer = {}; // Provide required input
+    const result = await getMintAccounts({ payer });
+    // Add assertions to check the result
+  });
+
+  it('should create a token account', async () => {
+    const payer: Signer = {}; // Provide required input
+    const mintAddress: PublicKey = {}; // Provide required input
+    const ownerAddress: PublicKey = {}; // Provide required input
+    const result = await createTokenAccount({ payer, mintAddress, ownerAddress });
+    // Add assertions to check the result
+  });
+
+  it('should mint a token', async () => {
+    const payer: Signer = {}; // Provide required input
+    const mintAddress: PublicKey = {}; // Provide required input
+    const ownerAddress: PublicKey = {}; // Provide required input
+    const year: number = 2023; // Provide required input
+    const uri: string = 'sample_uri'; // Provide required input
+    const result = await mintToken({ payer, mintAddress, ownerAddress, year, uri });
+    // Add assertions to check the result
+  });
+
+  it('should get NFTs', async () => {
+    const ownerAddress: PublicKey = {}; // Provide required input
+    const result = await getNFTs({ ownerAddress });
+    // Add assertions to check the result
+  });
+});
